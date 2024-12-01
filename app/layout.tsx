d15806892de7cd5c88fb003441347e065f89d71f
jbeat <jbeat0630@gmail.com>
2024-12-01 12:03:01 +0900

    feat: add default layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "@/components/(layout)/Header";
+import Footer from "@/components/(layout)/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+      <Header />
+        <main className="bg-primary">
+          {children}
+        </main>
+      <Footer />
       </body>
     </html>
   );
